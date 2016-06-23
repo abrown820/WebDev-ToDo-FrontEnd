ed3b1cdcea1cf34d5010174c82cc21ca9f650308
Alex <ajcbrown820@gmail.com>
2016-06-23 16:34:25 +0930

    Initial commit

---
+++ src/js/actions/ToDoActions.js
@@ -0,0 +1,32 @@
+import * as types from '../constants/ActionTypes';
+
+let nextToDoID = 0;
+
+export function addToDo(description) {
+  return {
+    type: types.ADD_TODO,
+    id: nextToDoID++,
+    description
+  };
+}
+
+export function deleteToDo(id) {
+  return {
+    type: types.DELETE_TODO,
+    id
+  };
+}
+
+export function toggleToDo(id) {
+  return {
+    type: types.TOGGLE_TODO,
+    id
+  };
+}
+
+export function setVisibilityFilter(filter) {
+  return {
+    type: types.SET_VISIBILITY_FILTER,
+    filter
+  };
+}
