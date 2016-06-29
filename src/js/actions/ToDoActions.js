a37d7b9ded8dc95e89801e68933b35a6a727148d
Alex <ajcbrown820@gmail.com>
2016-06-29 20:10:19 +0930

    Action Types for API Calls

--- src/js/actions/ToDoActions.js
+++ src/js/actions/ToDoActions.js
@@ -1,16 +1,51 @@
 import * as types from '../actionTypes/ActionTypes';
 
-let nextToDoID = 0;
-
-export function addToDo(description) {
+// Request Todos Actions
+export function RequestToDos(todos) {
   return {
-    type: types.ADD_TODO,
-    id: nextToDoID++,
-    description,
-    completed: false
+    type: types.REQUEST_TODOS,
+    todos
+  }
+}
+
+export function RequestToDosSuccess(todos) {
+  return {
+    types: types.REQUEST_TODOS_SUCCESS,
+    todos
   };
 }
 
+export function RequestTodosFailure(error){
+  return {
+    types: types.REQUEST_TODOS_FAILURE,
+    error
+  }
+}
+
+// Add Todo Actions
+export function AddToDo(description) {
+  return {
+    type: types.ADD_TODO_REQUEST,
+    description,
+    completed: false
+  }
+}
+
+export function AddToDoSuccess(id){
+  return {
+    type: types.ADD_TODO_SUCCESS,
+    id
+  }
+}
+
+export function AddToDoFailure(id){
+  return {
+    type: types.ADD_TODO_FAILURE,
+    id
+  }
+}
+
+// Delete Todo Actions
 export function deleteToDo(id) {
   return {
     type: types.DELETE_TODO,
@@ -18,13 +53,46 @@ export function deleteToDo(id) {
   };
 }
 
-export function toggleToDo(id) {
+export function deleteToDoSuccess(id) {
   return {
-    type: types.TOGGLE_COMPLETE,
+    type: types.DELETE_TODO_SUCCESS,
     id
   };
 }
 
+export function deleteToDoFailure(id){
+  return {
+    type: types.DELETE_TODO_FAILURE,
+    id
+  };
+}
+
+// Toggle Completion of Todo Actions
+export function toggleToDo(id, completed) {
+  return {
+    type: types.TOGGLE_COMPLETE_REQUEST,
+    id,
+    completed
+  };
+}
+
+export function toggleToDoSuccess(id, completed) {
+  return {
+    type: types.TOGGLE_COMPLETE_SUCCESS,
+    id,
+    completed
+  };
+}
+
+export function toggleToDoFailure(id, completed) {
+  return {
+    type: types.TOGGLE_COMPLETE_FAILURE,
+    id,
+    completed
+  };
+}
+
+// Visibility Filers
 export function setVisibilityFilter(filter) {
   return {
     type: types.SET_VISIBILITY_FILTER,
--- src/js/reducers/Reducers.js
+++ src/js/reducers/Reducers.js
@@ -1,6 +1,7 @@
 import { ADD_TODO, DELETE_TODO, TOGGLE_TODO, SET_VISIBILITY_FILTER, VisibilityFilters } from '../actionTypes/ActionTypes.js'
 import { combineReducers } from 'redux';
 const {SHOW_ALL} = VisibilityFilters;
+import expect from 'expect';
 
 // Reducer to handle visibilityFilter action
 function visibilityFilter(state = 'SHOW_ALL', action) {
@@ -14,18 +15,18 @@ function visibilityFilter(state = 'SHOW_ALL', action) {
 // Reducer to handle ADD and TOGGLE ToDo actions
 function todos(state = [], action) {
 	switch (action.type) {
-		case 'ADD_TODO':
+		case 'ADD_TODO_SUCCESS':
 			return [...state, {
 				id: action.id,
 				description: action.description,
 				completed: false
 			}]
-		case 'DELETE_TODO':
+		case 'DELETE_TODO_SUCCESS':
 			return [
 				...state.slice(0, action.id),
 				...state.slice(action.id + 1)
 			]
-		case 'TOGGLE_COMPLETE':
+		case 'TOGGLE_COMPLETE_SUCCESS':
 			return state.map((todo, id) => {
 				if (id === action.id) {
 					return Object.assign({}, todo, {
