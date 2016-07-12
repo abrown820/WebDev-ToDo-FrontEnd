0333b3ca7a6006ebee86f7450ed6a11c35deff14
Alex <ajcbrown820@gmail.com>
2016-07-12 23:33:43 +0930

    add importance to reducer

--- src/js/reducers/todos.js
+++ src/js/reducers/todos.js
@@ -6,6 +6,7 @@ function todos(state = [], action) {
 			return [...state, {
 				id: action.id,
 				description: action.description,
+				importance: action.importance,
 				completed: false
 			}]
 		case 'DELETE_TODO_SUCCESS':
