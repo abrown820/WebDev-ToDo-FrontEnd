19d557d28774391e147ef1d63a8308fb9fecda57
Daniel Betteridge <danielrbetteridge@gmail.com>
2016-07-14 21:16:37 +0800

    styling and font change for readability, added text border on task description

--- src/js/containers/AddToDo.js
+++ src/js/containers/AddToDo.js
@@ -7,7 +7,7 @@ let AddToDo = ({ dispatch }) => {
 
   return (
     <div>
-      <form onSubmit={e => {
+      <form className="task-form" onSubmit={e => {
         e.preventDefault()
         if (!input.value.trim()) {
           return
@@ -15,15 +15,16 @@ let AddToDo = ({ dispatch }) => {
         dispatch(asyncaddToDo(input.value, parseInt(document.getElementById('importanceSelect').value)))
         input.value = ''
       }}>
-        <input ref={node => {
+      
+        <input className="task-input" ref={node => {
           input = node
         }} />
-        <select id='importanceSelect'>
+        <select className="task-select" id='importanceSelect'>
           <option value='1'>Normal</option>
           <option value='2'>Important</option>
           <option value='3'>Very Important</option>
         </select>
-        <button type="submit">
+        <button className="task-submit" type="submit">
           Add Todo
         </button>
       </form>
